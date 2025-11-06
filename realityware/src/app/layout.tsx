e5ce26cff1cb6f3526a86f4312edd38595659ba2
YoussefDevPro <youssef.pro.coding@gmail.com>
2025-11-06 19:53:56 +0100

    so uh, made the hub page, still things to imrpove, also added the themes and uh, idk, this is still a prototype :p

--- realityware/src/app/layout.tsx
+++ realityware/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SharedLayout from "../components/SharedLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,35 +18,11 @@ export const metadata: Metadata = {
   description: "Real problems. Real solutions.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
-        <div className="min-h-screen flex">
-          <aside className="w-72 bg-[#c7c2b5] border-green-500 border p-4 sticky top-10">
-            <nav className="h-full flex flex-col text-black text-center space-y-4">
-              <div className="text-3xl">Realityware</div> 
-              <ul className="text-xl">
-                <li className="my-8"><a href="/hub">Hub</a></li>
-                <li className="my-8"><a href="/gallery">Gallery</a></li>
-                <li className="my-8"><a href="/projects">Projects</a></li>
-                <li className="my-8"><a href="/voting">Voting</a></li>
-                <li className="my-8"><a href="/store">Store</a></li>
-                <li className="my-8"><a href="/world">World</a></li>
-              </ul>
-              <div className=""><a href="/profile">Profile</a></div>
-            </nav>
-          </aside>
-
-          <main className="flex-1 p-8">
-
-            {children}
-          </main>
-        </div>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SharedLayout>{children}</SharedLayout>
       </body>
     </html>
   );
--- realityware/src/pages/_app.tsx
+++ realityware/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
 import "../app/globals.css";
+import SharedLayout from "../components/SharedLayout";
 
-export default function App ({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SharedLayout>
+      <Component {...pageProps} />
+    </SharedLayout>
+  );
 }
