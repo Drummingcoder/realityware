1a83d3791483e2debc2d6b8bd7b6944648c31f78
Tim Tran <121896621+Drummingcoder@users.noreply.github.com>
2025-11-09 00:54:38 +0000

    Adding more and more elements for the homepage, still working on making it look good

--- realityware/src/pages/index.tsx
+++ realityware/src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,26 +15,33 @@ export default function Home() {
           Sign in with Slack!
         </a>
       </div> */}
-
-      <div className="fixed top-40 right-10 z-50 max-width max-w-xs px-4">
+      <div className="fixed top-[12vh] right-[14vw] z-50 max-width max-w-xs px-4">
         <Image src="/hackclublogo.png" alt="Hack Club logo" width={181} height={63} className="object-cover" />
       </div>
 
       <div className="min-h-screen flex items-center justify-center bg-background">
         <main className="max-w-3xl w-full p-8">
-          <h1 className="text-8xl font-bold mb-4 text-center text-foreground">Realityware</h1>
-          <p className="text-lg mb-6 text-primary text-center">Ship a solution to a problem of society, get a grant to build it!</p>
-          <p className="text-sm mb-6 text-secondary">Psst...You can even get prizes like 3D printers, Raspberry Pis, and stickers!</p>
+          <h1 className="text-8xl font-bold mb-8 text-center text-foreground">Realityware</h1>
+          <div className="flex justify-center">
+            <p className="inline-block text-3xl mb-3 text-primary whitespace-nowrap">Ship a solution to a problem of society, get a grant to build it!</p>
+          </div>
+          <p className="text-sm mb-12 text-secondary text-center">Psst...You can even get prizes like 3D printers, Raspberry Pis, and stickers!</p>
 
-          <section className="space-y-4">
-            <a className="block px-4 py-3 rounded text-foreground text-center" href="/gallery">RSVP here!</a>
+          <section className="space-y-4 mb-32 flex justify-center">
+            <a className="inline-block border border-neutral px-3 py-1 rounded text-foreground text-center text-secondary bg-accent hover:opacity-90" href="/hub">RSVP here!</a>
           </section>
 
           <section className="space-y-4">
-            <p className="block px-4 py-3 rounded text-foreground text-center">So, how will the event work?</p>
+            <p className="block px-4 py-3 rounded text-center">So, how will the event work?</p>
           </section>
           <section className="space-y-4">
             <p className="block px-4 py-3 bg-background rounded text-foreground">Step 1: Come up with an idea!</p>
+          </section>
+          <section className="space-y-4">
+            <p className="block px-4 py-3 bg-background rounded text-foreground">Step 2: Build a model!</p>
+          </section>
+          <section className="space-y-4">
+            <p className="block px-4 py-3 bg-background rounded text-foreground">Step 3: Code it out!</p>
           </section>
         </main>
       </div>
