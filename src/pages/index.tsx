891ea3b448cb1e6d6906c97f655f341f8df6461c
Sophia Duan <sophiayduan@gmail.com>
2025-12-03 13:55:09 -0500

    update faq to use props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,20 @@ import Head from "next/head";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
-export default function Home() {
-  const [faq1, showfaq1] = useState(false);
-  const [faq2, showfaq2] = useState(false);
-  const [faq3, showfaq3] = useState(false);
-  const [faq4, showfaq4] = useState(false);
-  const [faq5, showfaq5] = useState(false);
+export default function Home() { 
+  
+  const [showFaq, setShowFaq] = useState(null);
+
+  //For Nav bar
   const [showTitle, setShowTitle] = useState(false);
+
+
+  // const FAQ = ["Question", "Answer"];
+    const faq = [
+      { id: 1 , question: "Question1", answer: "Answer1"},
+      { id: 2, question: "Question2", answer: "Answer2" },
+      { id: 3, question: "Question3", answer: "Answer3" }
+    ];
 
   useEffect(() => {
     const handleScroll = () => {
@@ -34,26 +41,26 @@ export default function Home() {
       </div> */}
       <header className="">
           {showTitle && (
-            <div className="fixed top-0 left-0 w-full z-2 bg-background/80 backdrop-blur shadow text-left py-2 items-center flex">
-              <span className="text-[calc(2vw+2vh)] font-bold text-foreground ml-[2vw] text-left">Realityware</span>
+            <div className="fixed top-0 left-0 w-full z-2 bg-background/80 backdrop-blur shadow text-left py-2 items-center flex px-6">
+              <span className="text-[calc(2vw+2vh)] font-bold text-foreground text-left">Realityware</span>
              
-              <nav className="flex flex-row items-center flex-1 z-3 mr-[2vw]">
-                <ul className="flex flex-row space-x-[3vw] ml-auto">
-                  <li>
-                    <a href="#home" className="text-lg text-foreground hover:text-primary font-bold transition underline">Home</a>
-                  </li>
-                  <li>
-                    <a href="#about" className="text-lg text-foreground hover:text-primary font-bold transition underline">About</a>
-                  </li>
-                  <li>
-                    <a href="#faq" className="text-lg text-foreground hover:text-primary font-bold transition underline">FAQ</a>
-                  </li>
-                </ul>
-              </nav>
+              <nav className="w-full flex flex-row items-center justify-end  z-50">
+            <ul className="flex flex-row space-x-[3vw]">
+              <li>
+                <a href="#home" className="text-lg text-black hover:text-accent font-medium transition">Home</a>
+              </li>
+              <li>
+                <a href="#about" className="text-lg text-black hover:text-accent font-medium transition">About</a>
+              </li>
+              <li>
+                <a href="#faq" className="text-lg text-black hover:text-accent font-medium transition">FAQ</a>
+              </li>
+            </ul>
+          </nav>
             </div>
           )}
           
-          <nav className="absolute top-0 right-[2vw] w-full flex flex-row items-center justify-end py-4 z-50">
+          <nav className="absolute top-0 right-6 w-full flex flex-row items-center justify-end py-4 z-50">
             <ul className="flex flex-row space-x-[3vw]">
               <li>
                 <a href="#home" className="text-lg text-white hover:text-accent font-medium transition">Home</a>
@@ -76,7 +83,8 @@ export default function Home() {
           <div className="text-center space-y-6">
               <form name="rsvpForm" method="post" className="flex justify-between w-fit mx-auto gap-x-2">
                 <input type="email" placeholder="e.g.coolperson@gmail.com" className="text-primary font-medium bg-white p-4 pr-16 box-border h-12 rounded-lg"></input>
-                <input type="submit" value="RSVP" className="text-black font-semibold text-xl bg-lightaccent px-5 rounded-lg font-archivo"></input>
+                <input type="submit" value="RSVP" className="hidden md:block text-black font-semibold text-xl bg-lightaccent px-5 rounded-lg font-archivo"></input>
+                <input type="submit" value="->" className="md:hidden text-black font-semibold text-xl bg-lightaccent px-5 rounded-lg font-archivo"></input>
               </form>
               
             <p className="mx-auto w-90 max-w-[80%] text-center text-white leading-snug">Ship a solution to a problem of society, get a grant to build it! You can also get prizes like 3D printers, Raspberry Pis, and stickers!
@@ -84,16 +92,16 @@ export default function Home() {
           </div>
 
 
-          <div className="absolute bottom-8 right-8 rounded-lg bg-white/20 max-w-80 p-4 pb-8 flex justify-between">
-            <p className="text-white">Launching (insert some date here), January 2026!</p>
-            <span className="inline-flex rounded-full w-4 h-4 bg-red-500"></span>
+          <div className="absolute bottom-8 right-8 rounded-lg bg-white/10  max-w-80 p-4 pb-8 flex justify-between">
+            <span className="text-white text-md">Launching (insert some date here), January 2026!</span>
+            <span className="inline-flex rounded-full w-4 h-4 aspect-[1/1] bg-red-500"></span>
           </div>
         </section>
 
         <main className="w-full bg-topbackground mx-auto" id="about">
             {/* This can be worded better */}
             <h2 className="text-center my-10">HOW IT WORKS</h2>
-            <section className="px-8 lg:px-20 xl:px-30 grid grid-cols-4 grid-rows-2 gap-4">
+            <section className="px-8 lg:px-20 xl:px-30 md:grid grid-cols-4 grid-rows-2 gap-x-4 space-y-4">
               <div className="p-4 border border-2 border-black bg-neutral rounded-lg shadow-lg  text-lightblack overflow-y-auto">
                 <h3 className="text-center">Step 1: Come up with an idea!</h3>
                 <p>It can be anything, as well as a solution can be built for it! The better your idea is to help society, the better reward you will earn.</p>
@@ -154,123 +162,62 @@ h] portrait:w-[40vw] portrait:h-[25vh] p-4 border border-2 border-black bg-secon
          
           </section> */}
         
+          
+          <section className="px-8 lg:px-20 xl:px-30 bg-secondary flex flex-col items-center justify-center py-10" id="faq">
+            <h2 className="mb-10">FAQ</h2>
 
-          <section className="bg-secondary flex flex-col items-center justify-center" id="faq">
-            <h2 className="my-10">FAQ</h2>
-            <div className="relative bg-white pb-[2vh] w-[67vw] h-auto border border-black border-[0.25vw] shadow-lg rounded [clip-path:polygon(0%_0%,calc(100%-6vw)_0%,100%_6vw,100%_100%,0%_100%)]">
-              <div className="absolute -top-[0.25vw] -right-[0.25vw] pointer-events-none">
-                <div className="w-[6vw] h-[6vw] border-[0.25vw] border-black"></div>
+           
+            <div className="relative bg-white pb-[2vh] w-full lg:max-w-[67vw] h-auto border border-black border-2 shadow-lg rounded [clip-path:polygon(0%_0%,calc(100%-6vw)_0%,100%_6vw,100%_100%,0%_100%)] pt-8 sm:pt-20 lg:pt-30 px-4 md:px-8 xl:px-20 ">
+              <div className="absolute -top-1 -right-1 pointer-events-none ">
+                <div className="w-[6vw] h-[6vw] border-2 border-black"></div>
               </div>
-              <p
-                className="text-lightblack text-[calc(1.2vw+1vh)] mt-[6vh] ml-[2vw] cursor-pointer"
-                onClick={() => showfaq1((v) => !v)}
-              >
-                When will the YSWS start?
-                <span
-                  className={`absolute right-[2.5vw] transition-transform duration-200 inline-block ${
-                    faq1 ? "rotate-90" : ""
-                  }`}
-                >
-                  <span className="inline-block w-0 h-0 border-t-[1vw] border-t-transparent border-b-[1vw] border-b-transparent border-l-[1vw] border-l-black"></span>
-                </span>
-              </p>
+
+            {faq.map(item => (
+
+              <div key={item.id} >
+
+              <div className="flex justify-between items-center pt-2">
+                <p className="text-lightblack">
+                  {item.question}
+                </p>
+                <span className={`transition-transform duration-200 inline-block ${
+                    showFaq === item.id ? "rotate-90" : ""
+                  }`}>
+                  
+                  <span className="inline-block w-0 h-0 border-t-[1vw] border-t-transparent border-b-[1vw] border-b-transparent border-l-[1vw] border-l-black cursor-pointer" 
+                  onClick={() => setShowFaq(showFaq === item.id ? null : item.id)}></span>
+                  
+                </span>    
               
-              {faq1 && (
-                <p className="text-lightblack text-[calc(0.8vw+0.8vh)] mt-[4vh] ml-[2vw] transition-all duration-300 text-textlight">
-                  Around late December or early January!
+              </div>
+              
+              {showFaq === item.id && (
+                <p className="text-lightblack pl-2 transition-all duration-300 text-textlight">
+                  {item.answer}
                 </p>
               )}
-              <hr className="w-[63vw] mx-auto mt-[1vh]"/>
-              <p
-                className="text-lightblack text-[calc(1.2vw+1vh)] mt-[6vh] ml-[2vw] cursor-pointer"
-                onClick={() => showfaq2((v) => !v)}
-              >
-                Any special gimmicks?
-                <span
-                  className={`absolute right-[2.5vw] transition-transform duration-200 inline-block ${
-                    faq2 ? "rotate-90" : ""
-                  }`}
-                >
-                  <span className="inline-block w-0 h-0 border-t-[1vw] border-t-transparent border-b-[1vw] border-b-transparent border-l-[1vw] border-l-black"></span>
-                </span>
-              </p>
+              <hr className="w-full mt-2"/>
               
-              {faq2 && (
-                <p className="text-lightblack text-[calc(0.8vw+0.8vh)] mt-[4vh] ml-[2vw] transition-all duration-300 text-textlight">
-                  You will be placed into a city (out of 4 cities), and you must complete scenarios with your city to earn more points!
-                </p>
-              )}
-              <hr className="w-[63vw] mx-auto mt-[1vh]"/>
-              <p
-                className="text-lightblack text-[calc(1.2vw+1vh)] mt-[6vh] ml-[2vw] cursor-pointer"
-                onClick={() => showfaq3((v) => !v)}
-              >
-                Placeholder 1
-                <span
-                  className={`absolute right-[2.5vw] transition-transform duration-200 inline-block ${
-                    faq3 ? "rotate-90" : ""
-                  }`}
-                >
-                  <span className="inline-block w-0 h-0 border-t-[1vw] border-t-transparent border-b-[1vw] border-b-transparent border-l-[1vw] border-l-black"></span>
-                </span>
-              </p>
-              
-              {faq3 && (
-                <p className="text-lightblack text-[calc(0.8vw+0.8vh)] mt-[4vh] ml-[2vw] transition-all duration-300 text-textlight">
-                  Still working on the FAQ!
-                </p>
-              )}
-              <hr className="w-[63vw] mx-auto mt-[1vh]"/>
-              <p
-                className="text-lightblack text-[calc(1.2vw+1vh)] mt-[6vh] ml-[2vw] cursor-pointer"
-                onClick={() => showfaq4((v) => !v)}
-              >
-                Placeholder 2
-                <span
-                  className={`absolute right-[2.5vw] transition-transform duration-200 inline-block ${
-                    faq4 ? "rotate-90" : ""
-                  }`}
-                >
-                  <span className="inline-block w-0 h-0 border-t-[1vw] border-t-transparent border-b-[1vw] border-b-transparent border-l-[1vw] border-l-black"></span>
-                </span>
-              </p>
-              
-              {faq4 && (
-                <p className="text-lightblack text-[calc(0.8vw+0.8vh)] mt-[4vh] ml-[2vw] transition-all duration-300 text-textlight">
-                  Still working on the FAQ!
-                </p>
-              )}
-              <hr className="w-[63vw] mx-auto mt-[1vh]"/>
-              <p
-                className="text-lightblack text-[calc(1.2vw+1vh)] mt-[6vh] ml-[2vw] cursor-pointer"
-                onClick={() => showfaq5((v) => !v)}
-              >
-                Placeholder 3
-                <span
-                  className={`absolute right-[2.5vw] transition-transform duration-200 inline-block ${
-                    faq5 ? "rotate-90" : ""
-                  }`}
-                >
-                  <span className="inline-block w-0 h-0 border-t-[1vw] border-t-transparent border-b-[1vw] border-b-transparent border-l-[1vw] border-l-black"></span>
-                </span>
-              </p>
-              
-              {faq5 && (
-                <p className="text-lightblack text-[calc(0.8vw+0.8vh)] mt-[4vh] ml-[2vw] transition-all duration-300 text-textlight">
-                  Still working on the FAQ!
-                </p>
-              )}
-              <hr className="w-[63vw] mx-auto mt-[1vh]"/>
+              </div>
+                ))}
+
             </div>
+            
             <a className="block mt-10 mx-auto w-fit text-black font-semibold text-xl bg-lightaccent px-5 rounded-lg py-3 border-2 border-black" href="https://hackclub.slack.com/docs/T0266FRGM/F09S78K5M1P">LEARN MORE</a>
 
           </section>
 
         </main>
 
-        <footer className="bg-neutral w-full p-[4vh] mx-auto items-center justify-center flex">
-          <section className="space-y-4">
-            <p className="block px-[1vw] py-[2vh] rounded text-foreground text-[calc(0.9vw+1vh)]">Built with love by members of <a href="https://hackclub.com/" className="text-blue-900 underline">Hack Club</a>! View this website's <a href="https://github.com/Drummingcoder/realityware-site" className="text-blue-900 underline">source code</a> and join our <a href="https://hackclub.com/slack/" className="text-blue-900 underline">Slack!</a></p>
+        <footer className="bg-neutral w-full p-10 mx-auto items-center justify-center flex">
+          <section className="text-center space-y-2">
+            <h2>RW LOGO</h2>
+            <p className="">Built with love for teenagers, by teenagers! View this website's <a href="https://github.com/Drummingcoder/realityware-site" className="text-blue-900 underline">source code</a> and join our <a href="https://hackclub.com/slack/" className="text-blue-900 underline">Slack!</a></p>
+
+            <div className="flex gap-4 w-full justify-center ">
+              <a href="https://hackclub.com" className="text-blue-900 underline">Hack Club</a>
+              <a href="https://www.depot17.com" className="text-blue-900 underline">Depot17</a>
+            </div>
           </section>
         </footer>
     </>
