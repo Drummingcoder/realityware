cbe597ac53bf72ddc88f0386d6d1f566f14b3683
Tim Tran <121896621+Drummingcoder@users.noreply.github.com>
2025-12-24 18:03:06 +0000

    (feat) Combined the monorepo-migration branch and this branch, which means that a Rust barebones backend and Turborepo is in place

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  distDir: "../backend/static",
+  trailingSlash: true,
   images: {
+    unoptimized: true, // Required for static export
     remotePatterns: [
       {
         protocol: 'https',
