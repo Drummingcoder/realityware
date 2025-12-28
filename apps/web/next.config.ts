83c627935bc3c00ffe90ca1b334c0f82ede7fe07
Hexaa <me@hexagonn.my.id>
2025-12-28 15:45:11 +0700

    refactor(codebase): fix and optimize some codebases

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,41 +1,53 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "out",
   trailingSlash: true,
   images: {
-    unoptimized: true, // Required for static export
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.pinimg.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.pfps.gg',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.wikia.nocookie.net',
-        port: '',
-        pathname: '/**',
-      },
+      // for what?
+      // {
+      //   protocol: "https",
+      //   hostname: "i.pinimg.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "encrypted-tbn0.gstatic.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "cdn.pfps.gg",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "static.wikia.nocookie.net",
+      //   port: "",
+      //   pathname: "/**",
+      // },
     ],
   },
-  turbopack: {
-    root: "",
+  rewrites: () => {
+    const backendUrl = process.env.BACKEND_URL || "http://localhost:8000";
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backendUrl}/:path*`,
+      },
+      {
+        source: "/ph/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ph/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
   },
 };
 
